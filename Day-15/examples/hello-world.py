9cc39d944e8fc4f375083f80928a7bae55c62279
rajshekharns <100364409+rajshekharns@users.noreply.github.com>
2023-12-13 11:25:35 +0530

    Update hello-world.py

--- Day-15/examples/hello-world.py
+++ Day-15/examples/hello-world.py
@@ -3,6 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+#decorator is used to run action before the function execution
+#for example if user enter ip:port then incorrect path then it will send error msg path not found
+#before the execution of function decoraor is used perform certain actions.
 def hello_world():
     return 'Hello, World!'
 
